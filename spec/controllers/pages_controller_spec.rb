84f2209dcacbcfd27fc8b523e9d5bc40723e01c7
Jeff Stevens <jstevens12mwsu@gmail.com>
2010-07-17 23:59:29 -0500

    Added help page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -28,5 +28,12 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+  end
 
 end
